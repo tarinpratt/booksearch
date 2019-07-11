4820a0b371d5cad321e4473d052345460d9e2084
Tarin Pratt <tarinpratt@gmail.com>
2019-07-11 15:16:26 -0700

    header

---
+++ header/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from  'react';
+import './header.css';
+
+class Header extends Component {
+  render(){
+    return <header>
+      <h1>Google Book Search</h1>
+      </header>
+  }
+}
+
+export default Header;
