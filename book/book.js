a43e0c59c2780a7c7678fdf30aeb5fcf2d680f92
Tarin Pratt <tarinpratt@gmail.com>
2019-07-13 10:57:12 -0700

    final

--- book/book.js
+++ book/book.js
@@ -18,37 +18,35 @@ class Book extends Component {
                 <img className="imageLink" src={item.imageLinks.smallThumbnail} alt="book"/>
                 }
                 <div className="bookInfo">
-            <h2 className="bookTitle">
-            <a 
-            href={item.previewLink} 
-            target="_blank"
-            rel="noopener noreferrer"
-            >{item.title}
-            </a>
-             </h2>
-             {(item.hasOwnProperty('authors'))
-             &&
-             <h3 className="bookAuthor"> 
-            Author: {item.authors} 
-            </h3>
-             }
-            <h3 className="bookPrice">
-               
+                    <h2 className="bookTitle">
+                        <a 
+                        href={item.previewLink} 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        {item.title}
+                        </a>
+                    </h2>
+                {(item.hasOwnProperty('authors'))
+                 &&
+                <h3 className="bookAuthor"> 
+                Author: {item.authors} 
+                </h3>
+                }
+                <h3 className="bookPrice">
                 {(this.props.price[i].saleability !== 'FREE' 
                 && this.props.price[i].saleability !== 'NOT_FOR_SALE'
                  && this.props.price[i].hasOwnProperty('listPrice'))
                   &&
-            <Price bookPrice={this.props.price[i]}/>
+                 <Price bookPrice={this.props.price[i]}/>
                 }
-          </h3>
-            <div className="bookDescription">
-            {item.description}
-           </div>
-           </div>
-
-           </div>
+                </h3>
+                <div className="bookDescription">
+                {item.description}
+                </div>
+            </div>
+        </div>
         })}
-  
     </div>
   ) 
 }
