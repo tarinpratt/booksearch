483831855e1704581f815f222daa66661585b6c5
Tarin Pratt <tarinpratt@gmail.com>
2019-07-13 10:55:38 -0700

    final

--- book/book.js
+++ book/book.js
@@ -8,10 +8,16 @@ import Price from '../price/price';
 class Book extends Component {
     
     render(){
+    console.log(this.props.books)
   return (
     <div className="displayBooks">
         {this.props.books.map((item, i) => {
             return <div className="books" key={i}>
+               {(item.hasOwnProperty('imageLinks'))
+                  &&
+                <img className="imageLink" src={item.imageLinks.smallThumbnail} alt="book"/>
+                }
+                <div className="bookInfo">
             <h2 className="bookTitle">
             <a 
             href={item.previewLink} 
@@ -20,17 +26,26 @@ class Book extends Component {
             >{item.title}
             </a>
              </h2>
-             <div className="bookAuthor">
+             {(item.hasOwnProperty('authors'))
+             &&
+             <h3 className="bookAuthor"> 
             Author: {item.authors} 
-            </div>
-            <div className="bookPrice">
-               <Price 
-               bookPrice={this.props.price[i]}/>
-          </div>
+            </h3>
+             }
+            <h3 className="bookPrice">
+               
+                {(this.props.price[i].saleability !== 'FREE' 
+                && this.props.price[i].saleability !== 'NOT_FOR_SALE'
+                 && this.props.price[i].hasOwnProperty('listPrice'))
+                  &&
+            <Price bookPrice={this.props.price[i]}/>
+                }
+          </h3>
             <div className="bookDescription">
             {item.description}
            </div>
-           <img className="imageLink" src={item.imageLinks.smallThumbnail} alt="book"/>
+           </div>
+
            </div>
         })}
   
